f6163ca1052cccf7a26df8999d677dae8c4cb44a
Linh Ngo <lngo@clemson.edu>
2015-11-17 13:32:58 -0500

    Twitter Kafka Python

---
+++ repos/python/consumer.py
@@ -0,0 +1,12 @@
+from kafka.client import KafkaClient
+from kafka.consumer import SimpleConsumer
+
+kafka = KafkaClient("localhost:9092")
+
+print("After connecting to kafka")
+
+consumer = SimpleConsumer(kafka, "my-group", "test")
+
+for message in consumer:
+    print(message)
+
---
+++ repos/python/producer.py
@@ -0,0 +1,32 @@
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+from datetime import datetime
+
+from TwitterAPI import TwitterAPI
+
+TRACK_TERM = 'Clemson'
+
+CONSUMER_KEY = 'yXmWAkCKfUTXpxakMqJxy8NuH'
+CONSUMER_SECRET = 'xBfst9PYhkpwv3X3WdhuBsJHwf9fUEBuHPGz1NJikjw025odDh'
+ACCESS_TOKEN_KEY = '374741989-xGscbglKIEFMY7MTch2vPF6qsOKsV3U7qt24k0wb'
+ACCESS_TOKEN_SECRET = 'QWLwpPFGsZ2pwbe3dxxAsLzTl9s0jF1qeQjSbl0vZcG5g'
+
+
+api = TwitterAPI(CONSUMER_KEY,
+                 CONSUMER_SECRET,
+                 ACCESS_TOKEN_KEY,
+                 ACCESS_TOKEN_SECRET)
+
+r = api.request('statuses/filter', {'track': TRACK_TERM})
+
+kafka = KafkaClient("localhost:9092")
+
+producer = SimpleProducer(kafka)
+
+for item in r:
+  if 'text' in item:
+    print(item['text'])
+    Tweet_content = str(item['text'].encode("utf-8"))
+    producer.send_messages("test", Tweet_content + str(datetime.now().time()) )
+#  else:
+#    producer.send_messages("test", item + str(datetime.now().time()) )
