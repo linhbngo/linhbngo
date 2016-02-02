a7be937dd76a8c6134e01ad5ffbf5bcc8b8e5542
Linh Ngo <lngo@clemson.edu>
2016-02-02 07:56:45 -0500

    add pSimTwit

---
+++ repos/python/pSimTwit.py
@@ -0,0 +1,44 @@
+import sys
+import logging
+import json
+import gzip
+import os
+
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+from datetime import datetime
+
+#logging.basicConfig(
+#  format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+#  level=logging.DEBUG
+#)
+
+if len(sys.argv) != 4:
+  print("Usage: producer.py <broker:port> <topic> <path to directory containing data files>")
+  print("Note: use port 9092 for default Kafka broker and 6667 for HDP Kafka broker")
+  exit(-1)
+
+#kafka = KafkaClient(sys.argv[1])
+#producer = SimpleProducer(kafka)
+
+path = sys.argv[3]
+i = 1
+for filename in os.listdir(path):
+  print filename
+  for jsonLine in open(os.path.join(path,filename), "r"):
+    try:
+      d = json.loads(jsonLine)
+#      print i
+#      i += 1
+    except ValueError:
+      print "Ooops"
+      print "-" + jsonLine + "-"
+#  with gzip.open(os.path.join(path,filename), "rb") as f:
+#    print f
+#    twitData = json.loads(f.read().decode("ascii"))
+#    print twitData
+#  if 'text' in item:
+#    print(str(i) + " " + item['text'])
+#    Tweet_content = str(i) + " " + str(item['text'].encode("utf-8"))
+#    producer.send_messages(sys.argv[3], Tweet_content + str(datetime.now().time()) )
+#    i += 1
