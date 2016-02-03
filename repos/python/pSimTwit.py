f96804a1234344f5ff25933cb7235e228f75435c
Linh Ngo <lngo@clemson.edu>
2016-02-03 10:20:03 -0500

    more edits on pTwitSim

--- repos/python/pSimTwit.py
+++ repos/python/pSimTwit.py
@@ -1,7 +1,8 @@
 import sys
 import logging
 import json
-import gzip
+import datetime
+import dateutil.parser
 import os
 
 from kafka.client import KafkaClient
@@ -18,27 +19,38 @@ if len(sys.argv) != 4:
   print("Note: use port 9092 for default Kafka broker and 6667 for HDP Kafka broker")
   exit(-1)
 
-#kafka = KafkaClient(sys.argv[1])
-#producer = SimpleProducer(kafka)
+kafka = KafkaClient(sys.argv[1])
+producer = SimpleProducer(kafka)
 
 path = sys.argv[3]
 i = 1
+start_date = ''
+end_date = ''
+sleep_time = 0
 for filename in os.listdir(path):
   print filename
   for jsonLine in open(os.path.join(path,filename), "r"):
     try:
       d = json.loads(jsonLine)
-#      print i
-#      i += 1
-    except ValueError:
-      print "Ooops"
+      jd = json.dumps(d)
+      print (str(i) + " " + d['postedTime'])
+      if i == 1:
+        sleep_time = 0
+        start_date = dateutil.parser.parse(d['postedTime'])
+      else:
+        end_date = dateutil.parser.parse(d['postedTime'])
+        delta = end_date - start_date
+        sleep_time = delta.total_seconds()
+        start_date = end_date
+        print(sleep_time)
+      i += 1
+      producer.send_messages(sys.argv[2], jd)
+    except ValueError, ReaderError:
+      print "Ooops, bad line"
       print "-" + jsonLine + "-"
 #  with gzip.open(os.path.join(path,filename), "rb") as f:
 #    print f
 #    twitData = json.loads(f.read().decode("ascii"))
 #    print twitData
 #  if 'text' in item:
-#    print(str(i) + " " + item['text'])
-#    Tweet_content = str(i) + " " + str(item['text'].encode("utf-8"))
-#    producer.send_messages(sys.argv[3], Tweet_content + str(datetime.now().time()) )
-#    i += 1
+#    print(str(i) + " " + item[
