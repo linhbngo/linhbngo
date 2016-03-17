3e8768b8b09c9ff559d3105b786be8dcde2eda23
Linh Ngo <lngo@clemson.edu>
2016-03-17 09:28:16 -0400

    minor edits ...

--- java/Kafka-Streaming-Twitter/src/main/java/org/lngo/kafkastreamingtwitter/TwitterJavaConsumerToHBase.java
+++ java/Kafka-Streaming-Twitter/src/main/java/org/lngo/kafkastreamingtwitter/TwitterJavaConsumerToHBase.java
@@ -1,4 +1,4 @@
-package com.saurzcode.twitter;
+package kafkastreamingtwitter;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
--- java/Kafka-Streaming-Twitter/src/main/java/org/lngo/kafkastreamingtwitter/TwitterJavaProducer.java
+++ java/Kafka-Streaming-Twitter/src/main/java/org/lngo/kafkastreamingtwitter/TwitterJavaProducer.java
@@ -1,4 +1,4 @@
-package com.saurzcode.twitter;
+package org.lngo.kafkastreamingtwitter;
 
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
@@ -16,59 +16,102 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
-public class TwitterKafkaProducer {
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+public class TwitterKafkaProducer extends Configured implements Tool{
 
 	private static final String topic = "twitter-topic";
 
-	public static void run(String consumerKey, String consumerSecret,
-			String token, String secret) throws InterruptedException {
+  
+  public static void main(String[] args) {
+    try {
+      TwitterKafkaProducer.run(args[0], args[1], args[2], args[3]);
+    } catch (InterruptedException e) {
+      System.out.println(e);
+    }
+  }
 
-		Properties properties = new Properties();
-		properties.put("metadata.broker.list", "localhost:9092");
-		properties.put("serializer.class", "kafka.serializer.StringEncoder");
-		properties.put("client.id","camus");
-		ProducerConfig producerConfig = new ProducerConfig(properties);
-		kafka.javaapi.producer.Producer<String, String> producer = new kafka.javaapi.producer.Producer<String, String>(
-				producerConfig);
+ public int run(String[] args) throws Exception {
+    Path inputPath = new Path(args[0]);
 
-		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
-		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-		// add some track terms
-		endpoint.trackTerms(Lists.newArrayList("twitterapi",
-				"#AAPSweep"));
+    Configuration conf = getConf();
+    Job job = new Job(conf, this.getClass().toString());
 
-		Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
-				secret);
-		// Authentication auth = new BasicAuth(username, password);
+    FileInputFormat.setInputPaths(job, inputPath);
 
-		// Create a new BasicClient. By default gzip is enabled.
-		Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
-				.endpoint(endpoint).authentication(auth)
-				.processor(new StringDelimitedProcessor(queue)).build();
+    job.setJarByClass(TwitterKafkaProducer.class);
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
 
-		// Establish a connection
-		client.connect();
+    job.setMapperClass(Map.class);
 
-		// Do whatever needs to be done with messages
-		for (int msgRead = 0; msgRead < 1000; msgRead++) {
-			KeyedMessage<String, String> message = null;
-			try {
-				message = new KeyedMessage<String, String>(topic, queue.take());
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			producer.send(message);
-		}
-		producer.close();
-		client.stop();
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
 
-	}
+  public static class Map extends Mapper<Object, Text, Text, IntWritable> {
+  
+    public void setup(Context context) {
+        Configuration config = context.getConfiguration();
+        // get the following parameters from -D generic Options:
+        String topic = config.get("mapper.word");
+        String zookeeper = ;
 
-	public static void main(String[] args) {
-		try {
-			TwitterKafkaProducer.run(args[0], args[1], args[2], args[3]);
-		} catch (InterruptedException e) {
-			System.out.println(e);
-		}
-	}
+        word.set(wordstring);
+    }
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      String consumerKey = tokenizer[0];
+      String consumerSecret = tokenizer[1];
+			String token = tokenizer[2];
+      String secret = tokenizer[3];
+
+  		Properties properties = new Properties();
+		  properties.put("metadata.broker.list", "localhost:9092");
+		  properties.put("serializer.class", "kafka.serializer.StringEncoder");
+		  properties.put("client.id","camus");
+
+		  ProducerConfig producerConfig = new ProducerConfig(properties);
+		  kafka.javaapi.producer.Producer<String, String> producer = new kafka.javaapi.producer.Producer<String, String>(producerConfig);
+
+		  BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+		  StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+		  // add some track terms
+		  endpoint.trackTerms(Lists.newArrayList("twitterapi","#AAPSweep"));
+
+		  Authentication auth = new OAuth1(consumerKey, consumerSecret, token,secret);
+		  
+      // Create a new BasicClient. By default gzip is enabled.
+		  Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
+			    .endpoint(endpoint).authentication(auth)
+				  .processor(new StringDelimitedProcessor(queue)).build();
+
+		  // Establish a connection
+		  client.connect();
+
+		  // Do whatever needs to be done with messages
+		  for (int msgRead = 0; msgRead < 1000; msgRead++) {
+			  KeyedMessage<String, String> message = null;
+			  try {
+				  message = new KeyedMessage<String, String>(topic, queue.take());
+			  } catch (InterruptedException e) {
+				  e.printStackTrace();
+			  }
+			  producer.send(message);
+		  }
+		  producer.close();
+		  client.stop();
+    }
+  }
 }
