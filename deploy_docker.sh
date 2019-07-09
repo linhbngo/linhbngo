46e3f11885fd3a8b63769ca38a7f89bea6119bbe
Linh B Ngo <lngo@wcupa.edu>
2019-07-09 16:51:02 -0400

    Update deploy_docker.sh

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -3,5 +3,5 @@ echo $1
 rm -Rf _site/*
 echo ${PWD}
 rm -Rf _site
-docker run -v ${PWD}:/srv/jekyll -it jekyll/jekyll jekyll build --verbose
+docker run -v ${PWD}:/srv/jekyll -it jekyll/jekyll:stable jekyll build --verbose
 scp -r _site/* $1:~/public_html
